0d6648aaa0377556baed98d7d38cff580c9f6e14
Roland Galgoczi <g.roli1996@gmail.com>
2023-06-04 14:56:01 +0200

    [REFACTOR]

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -4,9 +4,11 @@ function Header() {
   const navigate = useNavigate();
 
   return (
-    <header onClick={() => navigate("/")}>
-      <img src="/favicon.ico" alt="blogImg" />
-      <h1>TcT Blog</h1>
+    <header>
+      <div className="logoContainer" onClick={() => navigate("/")}>
+        <img src="/favicon.ico" alt="blogImg" />
+        <h1>TcT Blog</h1>
+      </div>
     </header>
   );
 }
